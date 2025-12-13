9d1abe81025cdefc6b60128149f088680b29aa8b
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-12-13 14:38:01 +0800

    feat: add add-service and view-services pages with routing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -20,7 +20,7 @@ import ViewTours from './components/viewtours/viewtours.jsx';
 // Promos & Posters
 import AddPromo from './components/addpromo/addpromo.jsx';
 import ViewPromos from './components/viewpromos/viewpromos.jsx';
-import AddPoster from './components/addposter/addposter.jsx';       
+import AddPoster from './components/addposter/addposter.jsx';       
 import ViewPoster from './components/viewposter/viewposter.jsx'; 
 
 // Blogs
@@ -41,7 +41,11 @@ import ViewDeal from './components/viewdeals/viewdeals.jsx';
 import AddHotel from './components/addhotel/addhotel.jsx';
 import ViewHotels from './components/viewhotel/viewhotel.jsx';
 
-// --- SERVICE MANAGEMENT IMPORTS (PascalCase folders based on screenshot) ---
+// --- NEW SERVICE MANAGEMENT IMPORTS ---
+import AddService from './components/addservice/addservice.jsx';
+import ViewServices from './components/viewservice/viewservice.jsx';
+
+// --- SERVICE MANAGEMENT IMPORTS (PascalCase folders based on structure) ---
 import VisaProcessing from './components/services/VisaProcessing/VisaProcessing.jsx';
 import PSASerbilis from './components/services/PSASerbilis/PSASerbilis.jsx';
 import CenomarRequest from './components/services/CenomarRequest/CenomarRequest.jsx';
@@ -56,66 +60,70 @@ import MarriageCertificate from './components/services/MarriageCertificate/Marri
 import Users from './components/users/users.jsx';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/users" element={<Users />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/users" element={<Users />} />
 
-        {/* Packages */}
-        <Route path="/add-package" element={<AddPackage />} />
-        <Route path="/view-packages" element={<ViewPackages />} />
-        <Route path="/edit-package" element={<EditPackage />} />
-        
-        {/* Tours */}
-        <Route path="/add-tour" element={<AddTour />} />
-        <Route path="/view-tours" element={<ViewTours />} />
+        {/* Packages */}
+        <Route path="/add-package" element={<AddPackage />} />
+        <Route path="/view-packages" element={<ViewPackages />} />
+        <Route path="/edit-package" element={<EditPackage />} />
+        
+        {/* Tours */}
+        <Route path="/add-tour" element={<AddTour />} />
+        <Route path="/view-tours" element={<ViewTours />} />
 
-        {/* Promos & Posters */}
-        <Route path="/add-promo" element={<AddPromo />} />
-        <Route path="/view-promos" element={<ViewPromos />} />
-        <Route path="/add-poster" element={<AddPoster />} />       
-        <Route path="/view-posters" element={<ViewPoster />} /> 
+        {/* Promos & Posters */}
+        <Route path="/add-promo" element={<AddPromo />} />
+        <Route path="/view-promos" element={<ViewPromos />} />
+        <Route path="/add-poster" element={<AddPoster />} />       
+        <Route path="/view-posters" element={<ViewPoster />} /> 
 
-        {/* Blogs */}
-        <Route path="/add-blog" element={<AddBlog />} />
-        <Route path="/view-blogs" element={<ViewBlog />} />
+        {/* Blogs */}
+        <Route path="/add-blog" element={<AddBlog />} />
+        <Route path="/view-blogs" element={<ViewBlog />} />
 
-        {/* Image Gallery */}
-        <Route path="/add-image" element={<AddImage />} />
-        <Route path="/view-images" element={<ViewImage />} />
+        {/* Image Gallery */}
+        <Route path="/add-image" element={<AddImage />} />
+        <Route path="/view-images" element={<ViewImage />} />
 
-        {/* Deals */}
-        <Route path="/add-deals" element={<AddDeal />} />
-        <Route path="/view-deals" element={<ViewDeal />} />
-        
-        {/* Testimonials */}
-        <Route path="/view-testimonials" element={<ViewTestimonials />} />
-        <Route path="/add-testimonial" element={<AddTestimonial />} />
-        
-        {/* Hotel Inventory Management */}
-        <Route path="/add-hotel" element={<AddHotel />} />
-        <Route path="/view-hotels" element={<ViewHotels />} />
+        {/* Deals */}
+        <Route path="/add-deals" element={<AddDeal />} />
+        <Route path="/view-deals" element={<ViewDeal />} />
+        
+        {/* Testimonials */}
+        <Route path="/view-testimonials" element={<ViewTestimonials />} />
+        <Route path="/add-testimonial" element={<AddTestimonial />} />
+        
+        {/* Hotel Inventory Management */}
+        <Route path="/add-hotel" element={<AddHotel />} />
+        <Route path="/view-hotels" element={<ViewHotels />} />
 
-        {/* --- SERVICE MANAGEMENT ROUTES --- */}
-        <Route path="/services/visa" element={<VisaProcessing />} />
-        <Route path="/services/psa" element={<PSASerbilis />} />
-        <Route path="/services/cenomar" element={<CenomarRequest />} />
-        <Route path="/services/passport" element={<PassportAppt />} />
-        <Route path="/services/airlinebooking" element={<AirlineBooking />} />
-        <Route path="/services/hotelbooking" element={<HotelBooking />} />
-        <Route path="/services/tourarrangements" element={<TourArrangements />} />
-        <Route path="/services/ferrybooking" element={<FerryBooking />} />
-        <Route path="/services/marriagecert" element={<MarriageCertificate />} />
-        <Route path="/services/travelinsurance" element={<TravelInsurance />} />
-        <Route path="/services/billspayment" element={<BillsPayment />} />
+        {/* --- NEW SERVICE MANAGEMENT ROUTES --- */}
+        <Route path="/add-service" element={<AddService />} />
+        <Route path="/view-services" element={<ViewServices />} /> {/* PLACEHOLDER ROUTE */}
 
-      </Routes>
-    </BrowserRouter>
-  );
+        {/* --- SERVICE MANAGEMENT ROUTES --- */}
+        <Route path="/services/visa" element={<VisaProcessing />} />
+        <Route path="/services/psa" element={<PSASerbilis />} />
+        <Route path="/services/cenomar" element={<CenomarRequest />} />
+        <Route path="/services/passport" element={<PassportAppt />} />
+        <Route path="/services/airlinebooking" element={<AirlineBooking />} />
+        <Route path="/services/hotelbooking" element={<HotelBooking />} />
+        <Route path="/services/tourarrangements" element={<TourArrangements />} />
+        <Route path="/services/ferrybooking" element={<FerryBooking />} />
+        <Route path="/services/marriagecert" element={<MarriageCertificate />} />
+        <Route path="/services/travelinsurance" element={<TravelInsurance />} />
+        <Route path="/services/billspayment" element={<BillsPayment />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
