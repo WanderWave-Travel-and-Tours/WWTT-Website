652ee737c751ed01fd02b9457889c32f53ee0ffd
batmhannJ <reyeshannahjoy82@gmail.com>
2026-01-09 09:00:37 +0800

    style: moved services list under main menu section in the sidebar

--- admin/src/components/sidebar/sidebar.jsx
+++ admin/src/components/sidebar/sidebar.jsx
@@ -349,12 +349,6 @@ const Sidebar = ({ isCollapsed, toggleSidebar }) => {
                 { name: 'Testimonials', path: '/view-testimonials', icon: ClipboardList },
               ]}
             />
-          </ul>
-        </div>
-
-        <div className="menu-group">
-          <h3 className={`menu-title ${isCollapsed ? 'hidden' : ''}`}>Management</h3>
-          <ul className="nav-list">
             <DropdownMenu 
               title="Services" 
               icon={Wrench} 
@@ -379,6 +373,12 @@ const Sidebar = ({ isCollapsed, toggleSidebar }) => {
                 { name: 'Bills', path: '/services/billspayment', icon: DollarSign },
               ]}
             />
+          </ul>
+        </div>
+
+        <div className="menu-group">
+          <h3 className={`menu-title ${isCollapsed ? 'hidden' : ''}`}>Management</h3>
+          <ul className="nav-list">
             <MenuItem 
                 path="/seller-rate" 
                 icon={TrendingUp} 
